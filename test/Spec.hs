dbe2e18ba58a60389f5173f73ea7b921e5380984
Toby Smyth <tobs169@gmail.com>
2016-02-17 09:40:11 +0000

    Hide successes

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,10 @@
 
-import qualified Problems.Lists as Lists
+import qualified Problems.Lists               as Lists
 import           Test.Tasty
-
+import           Test.Tasty.Ingredients.Basic
+import           Test.Tasty.Options
 main :: IO ()
-main = defaultMain tests
+main = defaultMain (localOption (HideSuccesses True) tests)
 
 tests :: TestTree
 tests = testGroup "99 Problems" $ reverse [Lists.tests]
