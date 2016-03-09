53803db00402bf9ce0137724931b24d1712666c0
Toby Smyth <toby.smyth@red-gate.com>
2016-03-09 14:14:19 +0000

    Types and stubs

--- src/Solutions/Arithmetic.hs
+++ src/Solutions/Arithmetic.hs
@@ -4,3 +4,29 @@ where
 solution31 :: Integral a => a -> Bool
 solution31 = undefined
 
+solution32 :: Integral a => a -> a -> a
+solution32 = undefined
+
+solution33 :: Integral a => a -> a -> Bool
+solution33 = undefined
+
+solution34 :: Integral a => a -> a
+solution34 = undefined
+
+solution35 :: Integral a => a -> [a]
+solution35 = undefined
+
+solution36 :: Integral a => a -> [(a, a)]
+solution36 = undefined
+
+solution37 :: Integral a => a -> a
+solution37 = undefined
+
+solution39 :: Integral a => a -> a -> [a]
+solution39 = undefined
+
+solution40 :: Integral a => a -> (a, a)
+solution40 = undefined
+
+solution41 :: Integral a => a -> a -> [(a, a)]
+solution41 = undefined
