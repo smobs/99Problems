88f180b497bb423d0432d591204da3de5f76a5c4
Toby Smyth <toby.smyth@red-gate.com>
2016-03-09 14:44:43 +0000

    Coprime

--- test/Problems/Arithmetic.hs
+++ test/Problems/Arithmetic.hs
@@ -9,13 +9,14 @@ tests :: TestTree
 tests = testGroup "Arithmetic properties" $ reverse
                   [ testProperty "31. is prime" $ (sameFunctionInt tIsPrime solution31)
                   , testProperty "32. Greatest common divisor" $ sameFunctionTwoInt (tGcd) (uncurry solution32)
+                  , testProperty "33. Coprime" $ sameFunctionTwoInt (tCoprime) (uncurry solution33)
                   ]
 
 sameFunctionInt :: (Eq a, Show a) => (Int -> a) -> (Int -> a) -> Int -> Bool
 sameFunctionInt = sameFunction
 
-sameFunctionTwoInt :: (Eq a, Show a) => ((Int, Int) -> a) -> ((Int, Int) -> a) -> (Int, Int)  -> Bool 
-sameFunctionTwoInt = sameFunction
+sameFunctionTwoInt :: (Eq a, Show a) => ((Int, Int) -> a) -> ((Int, Int) -> a) -> (Positive Int, Positive Int)  -> Bool 
+sameFunctionTwoInt f1 f2 (Positive x, Positive y) = sameFunction f1 f2 (x, y)
 
 tIsPrime :: Integral a => a -> Bool
 tIsPrime i | i < 2 = False 
@@ -26,3 +27,6 @@ tGcd  = uncurry f
      where 
      f a 0 = a
      f a b = f b (mod a b)
+
+tCoprime :: (Int, Int) -> Bool
+tCoprime = (1 == ) . tGcd
