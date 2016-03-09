3b0f5e1e2960bae244dbdc2a65688586cb97ee96
Toby Smyth <toby.smyth@red-gate.com>
2016-03-09 14:36:34 +0000

    Problem 32

--- test/Problems/Arithmetic.hs
+++ test/Problems/Arithmetic.hs
@@ -6,12 +6,23 @@ import Test.Tasty.QuickCheck
 import Utils
 
 tests :: TestTree
-tests = testGroup "Arithmetic properties" 
-                  [testProperty "31. is prime" $ (sameFunctionInt tIsPrime solution31)]
+tests = testGroup "Arithmetic properties" $ reverse
+                  [ testProperty "31. is prime" $ (sameFunctionInt tIsPrime solution31)
+                  , testProperty "32. Greatest common divisor" $ sameFunctionTwoInt (tGcd) (uncurry solution32)
+                  ]
 
 sameFunctionInt :: (Eq a, Show a) => (Int -> a) -> (Int -> a) -> Int -> Bool
 sameFunctionInt = sameFunction
 
+sameFunctionTwoInt :: (Eq a, Show a) => ((Int, Int) -> a) -> ((Int, Int) -> a) -> (Int, Int)  -> Bool 
+sameFunctionTwoInt = sameFunction
+
 tIsPrime :: Integral a => a -> Bool
 tIsPrime i | i < 2 = False 
            | otherwise = all (\x -> (mod i x) /= 0) [2 .. (div i 2)]
+
+tGcd :: (Int, Int) -> Int
+tGcd  = uncurry f
+     where 
+     f a 0 = a
+     f a b = f b (mod a b)
