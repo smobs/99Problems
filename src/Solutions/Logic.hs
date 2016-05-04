575c1a95254b08c3512abaf8b6695e60dc38c1df
Toby Smyth <toby.smyth@red-gate.com>
2016-05-04 13:11:30 +0100

    Huff this.

--- src/Solutions/Logic.hs
+++ src/Solutions/Logic.hs
@@ -1,6 +1,8 @@
 module Solutions.Logic 
 where
 
+import qualified Data.List as L
+  
 foo = "hlel"
 
 and2 :: Bool -> Bool -> Bool
@@ -55,3 +57,22 @@ grey 0 = [[]]
 grey n = (map (\x -> [True] ++ x) g ) ++ (map (\x -> [False] ++ x) (reverse g))
       where
          g = grey (n-1) 
+
+
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving Show
+
+huffman :: [(Char, Int)] -> [(Char, [Bool])]
+huffman  = L.sortBy (\(x,_) (y,_) -> compare x y) . unsorted
+    where 
+           unsorted = flip generateCode [] .  huffman' . sortHuffman . map (\(x, i) -> (Leaf x, i))
+
+huffman' :: [(Tree Char, Int)] -> Tree Char
+huffman' [(x, _)] = x
+huffman' ((x, u):(y, v):xs) = let newPair = (Node x y, u +v) in
+  huffman' . sortHuffman $ (newPair : xs)
+
+sortHuffman =  L.sortBy (\(_, i) (_,j) -> compare i j)
+
+generateCode :: Tree Char  -> [Bool] -> [(Char, [Bool])]
+generateCode (Leaf x) resultSoFar =[(x, resultSoFar)]
+generateCode (Node x y) resultsSoFar  = generateCode x (resultsSoFar ++ [False]) ++ generateCode y (resultsSoFar ++ [True]) 
